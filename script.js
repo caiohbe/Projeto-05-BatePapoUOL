493ebdc888a40b431061897ad8752cb3b111b3aa
caiohbe <caiohbe@gmail.com>
2022-08-26 14:58:01 -0300

    Refresh chat remade/sending msg added

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 const chat = document.querySelector('.chat');
 let lastMsg
+let to = 'Todos'
+
+function refreshChat() {
+    axios
+    .get('https://mock-api.driven.com.br/api/v6/uol/messages')
+    .then((requisition) => {
+        const msg = requisition.data;
+        const renderMsg = msg.map((thisMsg) => {
+        let html;
+        if (thisMsg.type === 'status') {
+            html = `
+            <div class="status">
+                <p><span>(${thisMsg.time}) </span><strong>${thisMsg.from} </strong>${thisMsg.text}</p> 
+            </div>
+            `;
+        } else {
+            html = `
+            <div class="${thisMsg.type}">
+                <p><span>(${thisMsg.time}) </span><strong>${thisMsg.from} </strong>para <strong>${thisMsg.to}: </strong>${thisMsg.text}</p>
+            </div>
+            `;
+        }
+        return html;
+        });
+    
+        if (lastMsg === renderMsg[99]) {
+            return;
+        } 
+        lastMsg = renderMsg[99];
+
+        chat.innerHTML = '';
+        for(let i = 0; i < 100; i++) {
+            chat.innerHTML += `${renderMsg[i]}`;
+        }
+        
+        chat.lastElementChild.scrollIntoView()
+    })
+}
+
 function login(button) {
     const name = document.querySelector('.login input').value;
 
@@ -28,48 +67,32 @@ function login(button) {
             .post('https://mock-api.driven.com.br/api/v6/uol/status', user);
         }, 5000); 
 
-        setInterval(() => {
-            axios
-            .get('https://mock-api.driven.com.br/api/v6/uol/messages')
-            .then((requisition) => {
-                const msg = requisition.data;
-                const renderMsg = msg.map((thisMsg) => {
-                let html;
-                if (thisMsg.type === 'status') {
-                    html = `
-                    <div class="status">
-                        <p><span>(${thisMsg.time}) </span><strong>${thisMsg.from} </strong>${thisMsg.text}</p> 
-                    </div>
-                    `;
-                } else {
-                    html = `
-                    <div class="${thisMsg.type}">
-                        <p><span>(${thisMsg.time}) </span><strong>${thisMsg.from} </strong>para <strong>${thisMsg.to}: </strong>${thisMsg.text}</p>
-                    </div>
-                    `;
-                }
-                return html;
-                });
-            
-                if (lastMsg === renderMsg[99]) {
-                    return;
-                } 
-                lastMsg = renderMsg[99];
-
-                console.log(renderMsg[99])
-                chat.innerHTML = '';
-                for(let i = 0; i < 100; i++) {
-                    chat.innerHTML += `${renderMsg[i]}`;
-                }
-                
-                chat.lastElementChild.scrollIntoView()
-            })
-        }, 3000);
+        setInterval(refreshChat, 3000);
     })
 
     requisition.catch((erro) => {
         if (erro.response.status === 400) {
-            alert('Nome já em uso! Tente outro.')
+            alert('Nome já em uso! Tente outro.');
         }
     })
+}
+
+function sendMsg() {
+    const textInput = document.querySelector('.footer input')
+    msgText = textInput.value;
+    textInput.value = ''
+
+    msgFrom = document.querySelector('.login input').value;
+    msgType = "message"
+
+    const msgObj = {
+        from: msgFrom,
+        to: to,
+        text: msgText,
+        type: msgType,
+    }
+
+    axios
+    .post('https://mock-api.driven.com.br/api/v6/uol/messages', msgObj)
+    .then(refreshChat)
 }
